1d4f182b0b75372240b7e5e2e0ab12edb02b44a8
EvanOleary26 <43943154+EvanOleary26@users.noreply.github.com>
2025-01-29 16:42:06 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,6 +48,12 @@ class BballPlayer {
 		void incPasMade() {
 			passesMade++;
 		}
+ 		void incShtTake() {
+			shotsTaken++;
+		}
+		void incShtMade() {
+			shotsMade++;
+		}
 
 
 		// funtions
@@ -76,7 +82,40 @@ class BballPlayer {
 				return false;
 			}
 		}
-		int TakeShot() {};
+		
+		 int TakeShot(int point) {
+            srand(time(0));
+
+            double shotPerc = 100 * static_cast<double>(shotsMade) / shotsTaken;
+            incShtAtt();
+
+            if (point == 3){
+                int randNum = (rand() % 71);
+                if (shotPerc > randNum){
+                    //std::cout << "made 3 point" << std::endl;
+                    incShtMade();
+                    return point;
+                }
+            } else if (point == 2) {
+                int randNum = (rand() % 101);
+                if (shotPerc > randNum){
+                    //std::cout << "made 2 point" << std::endl;
+                    incShtMade();
+                    return point;
+                }
+            } else if (point == 1) {
+                int randNum = (rand() % 126);
+                if (shotPerc > randNum){
+                    //std::cout << "made 1 point" << std::endl;
+                    incShtMade();
+                    return point;
+                }
+            } else {
+                return 0;
+            }
+
+            return 0;
+    }
 
 };
 
